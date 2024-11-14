2e0c6731ad7a7504c15abcd344c7ecf0eb295470
ericknovais <erick.hora@outlook.com>
2024-11-14 00:17:29 -0300

    Primeiro Array

--- src/bytebank_ATENDIMENTO/Program.cs
+++ src/bytebank_ATENDIMENTO/Program.cs
@@ -1,2 +1,20 @@
 ﻿Console.WriteLine("Boas Vindas ao ByteBank, Atendimento.");
 
+TesteArrayInt();
+
+void TesteArrayInt()
+{
+    Random rnd = new Random();
+    int[] idades = new int[5];
+
+    for (int i = 0; i < 5; i++)
+        idades[i] = rnd.Next(18, 90);
+
+    Console.WriteLine($"Tamanho do Array {idades.Length}");
+
+    for (int i = 0; i < idades.Length; i++)
+        Console.WriteLine($"indice [{i}] = {idades[i]}");
+
+    Console.WriteLine($"Valor total do Array {idades.Sum()}");
+    Console.WriteLine($"Valor da Media de idades {Convert.ToInt32(idades.Average())}");
+}
