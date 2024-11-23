dc24c3ffda2a28c1574409ffdf765e1a64357f1f
ericknovais <erick.hora@outlook.com>
2024-11-23 00:39:15 -0300

    Final do modulo 01 Array

--- src/bytebank_ATENDIMENTO/Program.cs
+++ src/bytebank_ATENDIMENTO/Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.WriteLine("Boas Vindas ao ByteBank, Atendimento.");
 
 //TesteArrayInt();
-TesteBuscaPalavra();
+//TesteBuscaPalavra();
 
 void TesteArrayInt()
 {
@@ -31,4 +31,35 @@ void TesteBuscaPalavra()
         Console.WriteLine($"Palavra encontrada = {busca}");
     else
         Console.WriteLine($"Palavra {busca} não foi encontrada ");
+};
+
+Array amostar = Array.CreateInstance(typeof(double), 5);
+amostar.SetValue(5.9, 0);
+amostar.SetValue(1.8, 1);
+amostar.SetValue(7.1, 2);
+amostar.SetValue(10, 3);
+amostar.SetValue(6.9, 4);
+
+TestaMediana(amostar);
+
+void TestaMediana(Array array)
+{
+    if ((array == null) || array.Length == 0)
+        Console.WriteLine("Array para calculo da mediana está vazio ou nulo");
+    else
+    {
+        double[] numerosOrdenados = (double[])array.Clone();
+        Array.Sort(numerosOrdenados);
+        int tamanho = numerosOrdenados.Length;
+        int meio = tamanho / 2;
+        double mediana = (tamanho % 2 != 0) ? numerosOrdenados[meio] :
+            (numerosOrdenados[meio] + numerosOrdenados[meio - 1]) / 2;
+        Console.WriteLine($"Com base na amostra a mediana = {mediana}");
+    }
+};
+
+int[] valores = { 10, 58, 36, 47 };
+for (int i = 0; i < valores.Length; i++)
+{
+    Console.WriteLine(valores[i]);
 };
